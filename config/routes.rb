5a331dacec4403bb3370932ee8baae24b0e6c5a6
yuukumaki <tadax2hitasurasusumudake@gmail.com>
2020-08-15 18:11:44 +0900

    automatic_update_01

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create, :update]
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json' }
+    end
   end
 end
