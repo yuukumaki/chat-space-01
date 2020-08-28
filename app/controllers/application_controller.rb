1ac7967f908ab37e152b6d2460d0e4e9cc8696c9
yuukumaki <tadax2hitasurasusumudake@gmail.com>
2020-08-28 21:52:16 +0900

    basic design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,23 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   protected
+
+  def production?
+    Rails.env.production?
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
+  
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,4 @@
 server '54.150.5.6', user: 'ec2-user', roles: %w{app db web}
+
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
